519834853e148f0b97969e45bd22bf98947909c0
Patrick Bessette <nuxian73@gmail.com>
2018-03-26 12:59:02 -0400

    Ajoutez des fichiers projet.

---
+++ SolarMag.com/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SolarMag.com.MvcApplication" Language="C#" %>
---
+++ SolarMag.com/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SolarMag.com.Startup))]
+namespace SolarMag.com
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
