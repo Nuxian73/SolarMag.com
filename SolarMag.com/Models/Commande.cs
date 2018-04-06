4b09798d3fa925eb7d04caa8d81f0c1a6b73f9ad
Patrick Lavergne <ftacorner@gmail.com>
2018-04-06 17:08:35 -0400

    modif commande

--- SolarMag.com/Models/Commande.cs
+++ SolarMag.com/Models/Commande.cs
@@ -2,10 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace SolarMag.com.Models
 {
     public class Commande
     {
+        public int Id { get; set; }
+       
+       [Display(Name = "No. Facture")]
+       
+        public string NoFacture { get; set; }
+
+        [Display(Name = "No. Produit")]
+         public string NoProduit { get; set; }
+
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        [Display(Name = "Quantité")]
+
+        public int Quantite{ get; set; }
+
+        
+       
+
+
+
+
+
+
     }
 }
