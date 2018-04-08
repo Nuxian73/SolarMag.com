bc5503d6e52bf60f31b9e90f3f2ffffc6e5a1669
atmmateau <1696032@cegepgranby.qc.ca>
2018-04-07 20:15:22 -0400

    client ajout

--- SolarMag.com/Models/Commande.cs
+++ SolarMag.com/Models/Commande.cs
@@ -9,28 +9,25 @@ namespace SolarMag.com.Models
     public class Commande
     {
         public int Id { get; set; }
-       
-       [Display(Name = "No. Facture")]
-       
+
+        public string NoPanier { get; set; }
+
+
+        [Display(Name = "No. Facture")]
         public string NoFacture { get; set; }
 
         [Display(Name = "No. Produit")]
-         public string NoProduit { get; set; }
+        public string NoProduit { get; set; }
 
         [Display(Name = "Description")]
         public string Description { get; set; }
 
         [Display(Name = "Quantité")]
-
         public int Quantite{ get; set; }
 
-        
-       
 
-
-
-
-
+        public virtual Item Items { get; set; }
+        public virtual Compte Comptes { get; set; }
 
     }
 }
