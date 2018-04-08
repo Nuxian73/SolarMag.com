2b36dede8bf8058d9bbb2af96a43ebe750e5f08c
atmateau <1696032@cegepgranby.qc.ca>
2018-04-07 21:38:13 -0400

    Revert "voila"
    This reverts commit 25360d8fd5361e7e4f8da1b39151b14cd0e16b8b.

--- SolarMag.com/Controllers/AccountController.cs
+++ SolarMag.com/Controllers/AccountController.cs
@@ -20,8 +20,6 @@ namespace SolarMag.com.Controllers
 
         public AccountController()
         {
-
-
         }
 
         public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
--- SolarMag.com/Models/Item.cs
+++ SolarMag.com/Models/Item.cs
@@ -9,23 +9,16 @@ namespace SolarMag.com.Models
     {
         public int Id { get; set; }
 
-
-        [Requires(ErrorMessage = "Nom de produit requit")]
-        [MaxLength(40, ErrorMessage = " maximum 40 caractères")]
         public string Nom { get; set; }
-
 
         public string Description { get; set; }
 
         public int Quantite { get; set; }
 
-        [RegularExpression(@"^\d+.\d{0,2}$", ErrorMessage = "La valeur décimale est de 2 chiffres après le point")]
-        [Range(0,5,ErrorMessage = "La valeur maximun est 5 chiffres")]
-        public Decimal Prix { get; set; }
+        public decimal Prix { get; set; }
 
-
-        //pour identifer les sous- produits  genre piles, panneau, accessoires et kit
-
-        public string Categorie { get;set; } 
+        public Item()
+        {
+        }
     }
 }
