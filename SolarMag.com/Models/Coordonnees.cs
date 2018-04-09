2946a093bc3f2423fd904c41ee6cf967b56a1058
atmateau <1696032@cegepgranby.qc.ca>
2018-04-09 12:53:22 -0400

    ajout coorddonnes

---
+++ SolarMag.com/Models/Coordonnees.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SolarMag.com.Models
+{
+    public class Coordonnees
+    {
+        public int NoCivique { get; set; }
+
+        public string Adresse { get; set; }
+
+        public string Ville { get; set; }
+
+        public string Province_Etat { get; set; }
+
+        public string Pays { get; set; }
+
+        public string CodePostal_ZipCode { get; set; }
+
+
+
+    }
+}
