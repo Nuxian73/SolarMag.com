9100f3c2ab2323b64f9d8e9ffb92c1ff0baa2b2a
Nuxian73 <nuxian73@gmail.com>
2018-04-13 14:29:53 -0400

    Modification du modèle Compte Ajout de l'objet Coordonndee

--- SolarMag.com/Models/Compte.cs
+++ SolarMag.com/Models/Compte.cs
@@ -5,13 +5,15 @@ using System.Web;
 
 namespace SolarMag.com.Models
 {
-    public class Compte : Coordonnee
+    public class Compte
     {
         public uint Id { get; set; }
 
         public string Nom { get; set; }
 
         public string Prenom { get; set; }
+
+        public Coordonnee Coordonnee { get; set; }
 
         public Compte()
         {
